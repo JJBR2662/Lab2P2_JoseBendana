c470065af15f509c1fc17653b1e3a13aa1deb1fa
JJBR2662 <bayernmunich2662@gmail.com>
2023-01-27 13:56:35 -0600

    Se crea el proyecto luego vamos con el menu principal y hacemos los dos arraylist uno de usuarios y otro de cosas que engloba casas solares y edificios

--- src/lab2p2_josebendaña/Lab2P2_JoseBendaña.java
+++ src/lab2p2_josebendaña/Lab2P2_JoseBendaña.java
@@ -1,9 +1,39 @@
 package lab2p2_josebendaña;
 
+import java.util.Scanner;
+
 public class Lab2P2_JoseBendaña {
 
     public static void main(String[] args) {
-        System.out.println("hola");
+        Scanner menu = new Scanner(System.in);
+        ArrayList<Usuario> = new ArraList();
+        int opcion = 0;
+        do{
+            System.out.println("""
+                               1- Registro de Inmueble/Solar
+                               2- Manejo de Estados
+                               3- Log In/Sign Up
+                               4- Salir""");
+            opcion = menu.nextInt();
+            switch(opcion){
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                case 4:
+                    System.out.println("Saliendooo............");
+                    break;
+                default:
+                    System.out.println("Opcion no valida, ingresela de nuevo");
+                    break;
+            }
+        }while(opcion!=4);
+        System.out.println("Se ha salido exitosamente :)");
     }
     
 }
